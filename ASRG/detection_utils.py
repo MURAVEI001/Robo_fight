52cdf5ea00a17981493f68e25fbc1beb51fe6494
Muravei <kirillgorbachev16@yandex.ru>
2026-07-27 10:23:52 +0300

    Сделан простой пример работы поиска связных компонентов

--- ASRG/detection_utils.py
+++ ASRG/detection_utils.py
@@ -3,25 +3,35 @@ import time
 import numpy as np
 
 def main():
-    cap = cv.VideoCapture(r"D:\GitHub\Robo_fight\ASRG\video2.mp4")
-    frame = cap.read()[1]
-    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    last_frame = np.zeros_like(gray_frame, dtype=frame.dtype)
+    #cap = cv.VideoCapture(r"D:\GitHub\Robo_fight\ASRG\video2.mp4")
+    cap = cv.VideoCapture(r"D:\GitHub\Robo_fight\ASRG\1.jpg")
+    _,frame = cap.read()
+    #cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    # frame = cap.read()[1]
+    #gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    last_frame = np.zeros_like(frame, dtype=frame.dtype)
+    gray_last_frame = cv.cvtColor(last_frame, cv.COLOR_BGR2GRAY)
+
 
     while True:
-        start = time.time()
-        _, frame = cap.read()
+        # start = time.time()
+        # _, frame = cap.read()
 
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        blur_frame = cv.GaussianBlur(gray_frame, ksize=(5,5), sigmaX=0)
+        diff_frame = cv.absdiff(gray_frame, gray_last_frame)
+        _, motion = cv.threshold(diff_frame, 25,255,cv.THRESH_BINARY)
+        # kernel = np.ones((3, 3), np.uint8)
+        # motion = cv.dilate(motion, kernel, iterations=2)
 
-        diff_frame = cv.absdiff(blur_frame, last_frame)
-        _, bin_frame = cv.threshold(diff_frame, 30,255,cv.THRESH_BINARY)
-        last_frame = blur_frame.copy()
-        contours, hierarchy = cv.findContours(bin_frame,cv.RETR_TREE,cv.CHAIN_APPROX_NONE)
-
-        cv.imshow("frame", bin_frame)
-        print(f"{time.time() - start:.4f}")
+        # last_frame = gray_frame.copy()
+        num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(motion, connectivity=8)
+        print(num_labels, "\n" , labels, "\n", stats, "\n", centroids)
+        input()
+        for i in range(num_labels):
+            cv.line(motion,pt1=(stats[i,0],stats[i,1]),pt2=(stats[i,0]+stats[i,2],stats[i,1]+stats[i,3]),color=(255,255,255),thickness=5 )
+            cv.line(motion,pt1=(int(centroids[i,0]),int(centroids[i,1])),pt2=(int(centroids[i,0]),int(centroids[i,1])),color=(0,0,0),thickness=10)
+        cv.imshow("frame", motion)
+        # print(f"{time.time() - start:.4f}")
 
         if cv.waitKey(30) == ord('q'):
             break
