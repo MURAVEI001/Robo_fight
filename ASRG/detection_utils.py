34622d093d0493e779b0d249aedacca52f3b3683
Muravei <kirillgorbachev16@yandex.ru>
2026-07-27 22:36:52 +0300

    создана отдельная функция фильтрации компонентов

--- ASRG/detection_utils.py
+++ ASRG/detection_utils.py
@@ -2,30 +2,37 @@ import cv2 as cv
 import time
 import numpy as np
 
+def filtered_componets(stats):
+    filtered_stats = []
+
+    for i, stat in enumerate(stats): 
+        if i == 0:
+            continue
+        if stat[4] >= 4000:
+            filtered_stats.append(stat)
+    return filtered_stats
+ 
 def main():
-    cap = cv.VideoCapture(r"C:\work_env\vs_code\Robo_fight\ASRG\video2.mp4")
-    #cap = cv.VideoCapture(r"D:\GitHub\Robo_fight\ASRG\1.jpg")
+    cap = cv.VideoCapture(r"ASRG\HZ_Kto.mp4")
     #cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    
     frame = cap.read()[1]
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     last_frame = np.zeros_like(gray_frame, dtype=frame.dtype)
-    # gray_last_frame = cv.cvtColor(last_frame, cv.COLOR_BGR2GRAY)
-
-
+    
     while True:
         start = time.time()
         _, frame = cap.read()
 
-        blur_frame = cv.GaussianBlur(frame,ksize=(9,9),sigmaX=0)
+        blur_frame = cv.GaussianBlur(frame,ksize=(9,9), sigmaX=0)
         gray_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2GRAY)
         diff_frame = cv.absdiff(gray_frame, last_frame)
-        _, motion = cv.threshold(diff_frame, 25,255,cv.THRESH_BINARY)
+        _, motion = cv.threshold(diff_frame, 25,255, cv.THRESH_BINARY)
         kernel = np.ones((3, 3), np.uint8)
         motion = cv.dilate(motion, kernel, iterations=3)
 
         last_frame = gray_frame.copy()
         num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(motion, connectivity=8)
-        #print(num_labels, "\n" , labels, "\n", stats, "\n", centroids)
         filtered_stats = []
         for i in range(num_labels):
             if i == 0:
@@ -34,7 +41,6 @@ def main():
                 filtered_stats.append(stats[i])
         for stats in filtered_stats:
             cv.rectangle(motion,pt1=(stats[0],stats[1]),pt2=(stats[0]+stats[2],stats[1]+stats[3]),color=(255,255,255),thickness=5 )
-            #cv.line(motion,pt1=(int(centroids[i,0]),int(centroids[i,1])),pt2=(int(centroids[i,0]),int(centroids[i,1])),color=(0,0,0),thickness=10)
         cv.imshow("frame", motion)
         print(f"{time.time() - start:.4f}")
 
--- ASRG/main.py
+++ ASRG/main.py
@@ -1,11 +1,16 @@
 import cv2 as cv
 import time
-from aruco_utils import initDetector,detectAruco,calcAngle
+import numpy as np
+from ASRG.aruco_utils import initDetector,detectAruco,calcAngle
+from ASRG.detection_utils import filtered_componets
 
 def main():
-    cap = cv.VideoCapture(0, cv.CAP_DSHOW)
-    # cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
-    # cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+    #cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    cap = cv.VideoCapture(r"ASRG\HZ_Kto.mp4")
+    frame = cap.read()[1]
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    last_frame = np.zeros_like(gray_frame, dtype=frame.dtype)
+
     detector = initDetector()
     while True:
         start = time.time()
@@ -16,8 +21,21 @@ def main():
                 if id == 0:
                     angle = calcAngle(corners[i])
                     #print(int(angle))
+        
+        blur_frame = cv.GaussianBlur(frame,ksize=(9,9), sigmaX=0)
+        gray_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2GRAY)
+        diff_frame = cv.absdiff(gray_frame, last_frame)
+        _, motion = cv.threshold(diff_frame, 25,255, cv.THRESH_BINARY)
+        kernel = np.ones((3, 3), np.uint8)
+        motion = cv.dilate(motion, kernel, iterations=3)
+        last_frame = gray_frame.copy()
+        num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(
+            motion, connectivity=8)
 
-        cv.imshow("frame", frame)
+        filtered_stats = filtered_componets(stats)
+        for stats in filtered_stats:
+            cv.rectangle(motion,pt1=(stats[0],stats[1]),pt2=(stats[0]+stats[2],stats[1]+stats[3]),color=(255,255,255),thickness=5 )
+        cv.imshow("frame", motion)
         print(f"{time.time() - start:.4f}")
 
         if cv.waitKey(30) == ord('q'):
