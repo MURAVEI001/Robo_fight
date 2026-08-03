0772b10affa6f5bc4b6f12f362e17d7d03bc2bb1
Muravei <kirillgorbachev16@yandex.ru>
2026-08-03 05:28:51 +0300

    добавлена отдельная функция отображения информации на изображении

--- ASRG/detection_utils.py
+++ ASRG/detection_utils.py
@@ -1,54 +1,22 @@
 import cv2 as cv
-import time
 import numpy as np
 
-def filtered_componets(stats):
+def filterComponets(stats, threshold):
     filtered_stats = []
 
     for i, stat in enumerate(stats): 
         if i == 0:
             continue
-        if stat[4] >= 4000:
+        if stat[4] > threshold:
             filtered_stats.append(stat)
     return filtered_stats
- 
-def main():
-    cap = cv.VideoCapture(r"ASRG\HZ_Kto.mp4")
-    #cap = cv.VideoCapture(0, cv.CAP_DSHOW)
-    
-    frame = cap.read()[1]
-    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    last_frame = np.zeros_like(gray_frame, dtype=frame.dtype)
-    
-    while True:
-        start = time.time()
-        _, frame = cap.read()
 
-        blur_frame = cv.GaussianBlur(frame,ksize=(9,9), sigmaX=0)
-        gray_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2GRAY)
-        diff_frame = cv.absdiff(gray_frame, last_frame)
-        _, motion = cv.threshold(diff_frame, 25,255, cv.THRESH_BINARY)
-        kernel = np.ones((3, 3), np.uint8)
-        motion = cv.dilate(motion, kernel, iterations=3)
+def getROI(frame,offset=0):
+    pass
 
-        last_frame = gray_frame.copy()
-        num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(motion, connectivity=8)
-        filtered_stats = []
-        for i in range(num_labels):
-            if i == 0:
-                continue
-            if stats[i,4] >= 4000:
-                filtered_stats.append(stats[i])
-        for stats in filtered_stats:
-            cv.rectangle(motion,pt1=(stats[0],stats[1]),pt2=(stats[0]+stats[2],stats[1]+stats[3]),color=(255,255,255),thickness=5 )
-        cv.imshow("frame", motion)
-        print(f"{time.time() - start:.4f}")
+def updateHistory(aruco=None,xySelf=None,xyEnemy=None):
+    pass
 
-        if cv.waitKey(30) == ord('q'):
-            break
-        
-    cap.release()
-    cv.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+def drawInfo(frame,angle=None,xySelf=None,xyEnemy=None, drawAngle=True, drawSelf=True, drawEnemy=True):
+    frame = cv.rectangle(frame,pt1=xySelf[0],pt2=xySelf[1],color=(0,0,255),thickness=5 )
+    return frame
--- ASRG/main.py
+++ ASRG/main.py
@@ -2,41 +2,46 @@ import cv2 as cv
 import time
 import numpy as np
 from ASRG.aruco_utils import initDetector,detectAruco,calcAngle
-from ASRG.detection_utils import filtered_componets
+from ASRG.detection_utils import filterComponets, drawInfo, getROI, updateHistory
 
 def main():
-    #cap = cv.VideoCapture(0, cv.CAP_DSHOW)
-    cap = cv.VideoCapture(r"ASRG\HZ_Kto.mp4")
-    frame = cap.read()[1]
+    cap = cv.VideoCapture(r"ASRG\2.mp4")
+    _, frame = cap.read()
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     last_frame = np.zeros_like(gray_frame, dtype=frame.dtype)
 
     detector = initDetector()
     while True:
         start = time.time()
-        _,frame = cap.read()
+        _, frame = cap.read()
+        aruco = False
         corners, idx, rejected = detectAruco(detector,frame)
         if not(idx is None):
             for i, id in enumerate(idx):
-                if id == 0:
+                if id == 47 :
                     angle = calcAngle(corners[i])
-                    #print(int(angle))
+                    aruco = True
         
         blur_frame = cv.GaussianBlur(frame,ksize=(9,9), sigmaX=0)
         gray_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2GRAY)
         diff_frame = cv.absdiff(gray_frame, last_frame)
-        _, motion = cv.threshold(diff_frame, 25,255, cv.THRESH_BINARY)
-        kernel = np.ones((3, 3), np.uint8)
-        motion = cv.dilate(motion, kernel, iterations=3)
+        _, threshold = cv.threshold(diff_frame, 25,255, cv.THRESH_BINARY)
+        kernel = np.ones((9, 9), np.uint8)
+        motion = cv.dilate(threshold, kernel, iterations=2)
         last_frame = gray_frame.copy()
         num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(
             motion, connectivity=8)
 
-        filtered_stats = filtered_componets(stats)
+        filtered_stats = filterComponets(stats, 4000)
+
         for stats in filtered_stats:
-            cv.rectangle(motion,pt1=(stats[0],stats[1]),pt2=(stats[0]+stats[2],stats[1]+stats[3]),color=(255,255,255),thickness=5 )
-        cv.imshow("frame", motion)
-        print(f"{time.time() - start:.4f}")
+            pt1 = np.array([stats[0],stats[1]])
+            pt2 = np.array([stats[0]+stats[2],stats[1]+stats[3]])
+            xySelf = np.array([pt1,pt2])
+            frame = drawInfo(frame, xySelf=xySelf, drawSelf=True)
+
+        cv.imshow("frame", frame)
+        #print(f"{time.time() - start:.4f}")
 
         if cv.waitKey(30) == ord('q'):
             break
