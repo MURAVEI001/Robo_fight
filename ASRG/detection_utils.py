a591d1af5587354e265483ae1fc80f34db85620f
Muravei <kirillgorbachev16@yandex.ru>
2026-08-12 17:18:43 +0300

    вычисление движения вынесено в отдельную функцию

--- ASRG/detection_utils.py
+++ ASRG/detection_utils.py
@@ -11,8 +11,11 @@ def filterComponents(stats, threshold):
             filtered_stats.append(stat)
     return filtered_stats
 
-def getROI(frame, points_self,offset=0):
+def getCentr(points_self):
     centr_point = points_self[2] + ((points_self[0] - points_self[2])//2)
+    return centr_point
+
+def getROI(centr_point, offset=0):
     ROI = np.array([centr_point - offset, centr_point + offset])
     return ROI
 
@@ -20,11 +23,15 @@ def drawAngle(frame,angle):
     frame = cv.putText(frame, text=f"Angle: {angle}",org=([40,60]),fontFace=1,fontScale=5,color=(0,0,255),thickness=5)
     return frame
 
-def drawSelf(frame,self_position):
-    frame = cv.line(frame, pt1=self_position[0],pt2=self_position[1],color=(0,0,255),thickness=3)
-    frame = cv.line(frame, pt1=self_position[1],pt2=self_position[2],color=(0,0,255),thickness=3)
-    frame = cv.line(frame, pt1=self_position[2],pt2=self_position[3],color=(0,0,255),thickness=3)
-    frame = cv.line(frame, pt1=self_position[3],pt2=self_position[0],color=(0,0,255),thickness=3)
-    frame = cv.line(frame, pt1=self_position[0],pt2=self_position[2],color=(255,0,0),thickness=3)
-
+def drawBox(frame,self_position):
+    frame = cv.rectangle(frame, pt1=(self_position[0]),pt2=(self_position[1]),color=(0,0,255),thickness=3)
     return frame
+
+def cycleDetection(frame, prev_frame):
+    blur_frame = cv.GaussianBlur(frame,ksize=(5,5), sigmaX=0)
+    diff_frame = cv.absdiff(blur_frame, prev_frame)
+    _, threshold = cv.threshold(diff_frame, 20,255, cv.THRESH_BINARY)
+    kernel = np.ones((3, 3), np.uint8)
+    motion = cv.dilate(threshold, kernel, iterations=4)
+    prev_frame = blur_frame.copy()
+    return motion, prev_frame
--- ASRG/main.py
+++ ASRG/main.py
@@ -2,64 +2,67 @@ import cv2 as cv
 import time
 import numpy as np
 from ASRG.aruco_utils import initDetector,detectAruco,calcAngle, getPointsMarker
-from ASRG.detection_utils import filterComponents, drawAngle, drawSelf, getROI
+from ASRG.detection_utils import filterComponents, drawAngle, drawBox, getROI, getCentr, cycleDetection
 from ASRG.fps import showFPS
+
+# НУЖНО ОПРЕДЕЛИТЬ
+# 1 ПОЛОЖЕНИЕ АРУКО
+# 2 УГОЛ РОБОТА
+# 3 ЕСЛИ АРУКО НЕ ОПРЕДЕЛЕНА, ТО ПОЛОЖЕНИЕ СЕБЯ
+# 4 ПОЛОЖЕНИЕ ПРОТИВНИКА
 
 def main():
     cap = cv.VideoCapture(r"ASRG\4.mp4")
-    _, frame = cap.read()
-    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    last_frame = np.zeros_like(gray_frame, dtype=frame.dtype)
 
     detector = initDetector()
 
     times = []
     angle = None
     points_self = None
+    prev_frame = None
     while True:
         start = time.time()
+
         _, frame = cap.read()
+        gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        if prev_frame is None:
+            prev_frame = np.zeros_like(gray_frame, dtype=gray_frame.dtype)
+
         aruco_flag = False
-        corners, ids, rejected = detectAruco(detector,frame)
+        corners, ids, rejected = detectAruco(detector, frame)
         if not(ids is None):
             for i, id in enumerate(ids):
                 if id == 47 :
-                    angle = calcAngle(corners[i])
+                    angle = calcAngle(corners[i]) # УГОЛ ПОВОРОТА
                     points_self = getPointsMarker(corners[i])
                     aruco_flag = True
                     break
 
         if aruco_flag:
-            ROI = getROI(frame,points_self, offset=100)
-            cv.rectangle(frame,pt1=(ROI[0]),pt2=(ROI[1]),color=(0,255,0),thickness=9)
+            centr_point = getCentr(points_self) # ПОЛОЖЕНИЕ АРУКО
+            # ROI = getROI(centr_point, offset=100)
+            # cv.rectangle(frame, pt1=(ROI[0]), pt2=(ROI[1]), color=(0,255,0), thickness=9)
 
             #frame = frame[ROI[0][1]:ROI[1][1], ROI[0][0]:ROI[1][0]]
-      
-        # blur_frame = cv.GaussianBlur(frame,ksize=(9,9), sigmaX=0)
-        # gray_frame = cv.cvtColor(blur_frame, cv.COLOR_BGR2GRAY)
-        # last_frame = np.zeros_like(gray_frame, dtype=frame.dtype)
 
-        # diff_frame = cv.absdiff(gray_frame, last_frame)
-        # _, threshold = cv.threshold(diff_frame, 60,255, cv.THRESH_BINARY)
-        # kernel = np.ones((3, 3), np.uint8)
-        # motion = cv.dilate(threshold, kernel, iterations=3)
-        # last_frame = gray_frame.copy()
-        # num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(
-        #     motion, connectivity=8)
+            motion, prev_frame = cycleDetection(gray_frame, prev_frame)
 
-        # filtered_stats = filterComponents(stats, 1200)
+            num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(
+                motion, connectivity=4)
 
-        # #frame = drawAngle(frame, angle)
-        # for stats in filtered_stats:
-        #     pt1 = np.array([stats[0],stats[1]])
-        #     pt2 = np.array([stats[0]+stats[2],stats[1]+stats[3]])
-        #     self_position = np.array([pt1,pt2])
-        # frame = drawSelf(frame, self_position=points_self)
+            filtered_stats = filterComponents(stats, 500)
 
-        cv.imshow("frame", frame)
-        #cv.imshow("frame1", threshold)
+        #frame = drawAngle(frame, angle)
+            for stats in filtered_stats:
+                pt1 = np.array([stats[0],stats[1]])
+                pt2 = np.array([stats[0]+stats[2],stats[1]+stats[3]])
+                enemy_position = np.array([pt1,pt2])
+                frame = drawBox(frame, enemy_position)
 
-        showFPS(times,start)
+            cv.imshow("frame", frame)
+            cv.imshow("frame1", motion)
+
+            showFPS(times,start)
 
         if cv.waitKey(30) == ord('q'):
             break
